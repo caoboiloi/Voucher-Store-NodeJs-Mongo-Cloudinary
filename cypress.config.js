88dac94cd5671242e09c8590461811341bb9f906
Loi Huynh <loihuynh.it@gmail.com>
2022-06-17 23:36:53 +0700

    add ci for cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('cypress')
+require('dotenv').config()
 
 module.exports = defineConfig({
   e2e: {
@@ -6,11 +7,18 @@ module.exports = defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
     supportFile: 'cypress/support/commands.js',
-    baseUrl: 'https://pdvoucher.azurewebsites.net/',
+    baseUrl: process.env.URL,
   },
   defaultCommandTimeout: 30000,
   pageLoadTimeout: 30000,
   responseTimeout: 30000,
   requestTimeout: 30000,
-  projectId: "56jv5t"
+  projectId: "5nzrh3",
+  env: {
+    user_admin: process.env.USER_ADMIN_CYPRESS,
+    pass_admin: process.env.PASS_ADMIN_CYPRESS,
+    user_customer: process.env.USER_CUSTOMER_CYPRESS,
+    pass_customer: process.env.PASS_CUSTOMER_CYPRESS,
+    login: process.env.LOGIN_CYPRESS
+  }
 })
